201417c6dfcd5421524e3494135a3a1bfe37bd47
Arius <70983185+Arius-Development@users.noreply.github.com>
2022-10-09 11:45:35 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,42 @@
+Config = {}
+
+
+
+Config.NPC = {
+    ["shop2"] = {
+        Pos = vector3(246.68, -664.56, 38.32),
+        Nome = "testing",
+        Modello = "a_f_m_ktown_02",
+        Blip = {
+            display = false,
+            type = 102,
+            grandezza = 0.8,
+            colore = 2,
+            nome = "downismo",
+        },
+        Items = {
+            {label = "burger", value = "burger", price = 20},
+            {label = "water", value = "water", price = 12},
+            {label = "armour", value = "armour", price = 1231},
+
+        }
+    },
+    ["shop3"] = {
+        Pos = vector3(159.44, -793.24, 31.12),
+        Nome = "testing",
+        Modello = "a_f_m_ktown_02",
+        Blip = {
+            display = true,
+            type = 102,
+            grandezza = 0.8,
+            colore = 2,
+            nome = "downismo",
+        },
+        Items = {
+            {label = "burger", value = "burger", price = 20},
+            {label = "water", value = "water", price = 12},
+            {label = "armour", value = "armour", price = 1231},
+
+        }
+    },
+}
