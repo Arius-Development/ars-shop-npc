df6c06f676b04e44c417d2efd60c6029a50c4398
AL1ƎN <72218928+johnsoul777@users.noreply.github.com>
2022-12-10 01:27:07 +0100

    Weight comprobation
    Weight comprobation

--- server/server.lua
+++ server/server.lua
@@ -6,14 +6,12 @@ RegisterNetEvent('ars-shop-npc:compraRoba', function(prezzo, item, label)
     local soldi = ox_inventory:Search(source, 'count', "money")
 
     if soldi > prezzo then
-        ox_inventory:RemoveItem(source, 'money', prezzo)
-        ox_inventory:AddItem(source, item, 1)
-        TriggerClientEvent('ox_lib:notify', source,
-            { title = 'Comprato ' .. label, style = { backgroundColor = '#141517', color = '#909296' },
-                icon = 'circle-check', iconColor = '#48BB78', type = 'success' })
+        if exports.ox_inventory:CanCarryItem(source, item, 1) then
+            ox_inventory:RemoveItem(source, 'money', prezzo)
+            ox_inventory:AddItem(source, item, 1)
+            TriggerClientEvent('ox_lib:notify', source, { title = 'Hai comprato ' .. label, style = { backgroundColor = '#141517', color = '#909296' }, icon = 'circle-check', iconColor = '#48BB78', type = 'success' })
+        end
     else
-        TriggerClientEvent('ox_lib:notify', source,
-            { title = 'Soldi insuffiscenti ' .. prezzo, style = { backgroundColor = '#141517', color = '#909296' },
-                icon = 'ban', iconColor = '#C53030', type = 'error' })
+        TriggerClientEvent('ox_lib:notify', source, { title = 'Soldi insuffiscenti ' .. prezzo, style = { backgroundColor = '#141517', color = '#909296' }, icon = 'ban', iconColor = '#C53030', type = 'error' })
     end
 end)
